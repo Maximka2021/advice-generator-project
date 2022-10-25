c102c5bf1c05ed697add0759ded6759672f2a497
Maxim Polyakov <polyakovmaxim05@gmail.com>
2022-10-25 11:27:32 -0400

    Commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const randomAdvice = document.querySelector("#random-advice");
+// const randomAdvice = document.querySelector("#random-advice");
 const userAdvice = document.querySelector("#user-advice");
 const newAdviceInput = document.querySelector("#new-advice-input");
 const newNameInput = document.querySelector("#new-name-input");
 const submitAdvice = document.querySelector("#submit-advice");
 
-fetch("https://api.adviceslip.com/advice")
-  .then((r) => r.json())
-  .then((data) => displayRandomAdvice(data));
+// fetch("https://api.adviceslip.com/advice")
+//   .then((r) => r.json())
+//   .then((data) => displayRandomAdvice(data));
 
 fetch("http://localhost:3000/new_advice")
   .then((r) => r.json())
   .then((data) => displayUserAdvice(data));
 
-function displayRandomAdvice(data) {
-  randomAdvice.textContent = data.slip.advice;
-}
+// function displayRandomAdvice(data) {
+//   randomAdvice.textContent = data.slip.advice;
+// }
 
 function displayUserAdvice(data) {
-  const arrayOfAdvices = data.map((item) => item.advice);
+  arrayOfAdvices = data.map((item) => item.advice);
   let randomAdvice =
     arrayOfAdvices[Math.floor(Math.random() * arrayOfAdvices.length)];
   userAdvice.textContent = randomAdvice;
@@ -27,16 +27,24 @@ submitAdvice.addEventListener("click", createNewAdvice);
 
 function createNewAdvice(e) {
   e.preventDefault()
-  const newData = {
-    name: newNameInput.value,
-    advice: newAdviceInput.value,
-  };
 
-  fetch("http://localhost:3000/new_advice", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newData),
-  });
+  arrayOfAdvices.forEach(elem => {
+    if(elem = newAdviceInput.value){
+      alert('Error')
+    }else{
+      const newData = {
+        name: newNameInput.value,
+        advice: newAdviceInput.value,
+      };
+    
+      fetch("http://localhost:3000/new_advice", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newData),
+      });
+    }
+  })
+
 }
